ea2388a5f73702e88771157e3113a5e64ef073be
fauzanazhari <fauzan.azharrahman04@gmail.com>
2025-09-18 18:57:21 +0800

    ubahk

--- api/flask-backend.py
+++ api/flask-backend.py
@@ -2,6 +2,6 @@ CORS(app, resources={
     r"/predict": {"origins": [
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://tanaman-herbal.vercel.app"
+        "https://herbalif.vercel.app"
     ]}
 })
